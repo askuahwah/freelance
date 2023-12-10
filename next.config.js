1632dd264ad4727867d581be8fd9e589f7d07ca7
askuahwah <askuahwah@gmail.com>
2023-12-10 21:29:29 +0530

    gasg

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   webpack(config, options) {
-    // Handle SVG files
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
-
-    // Return the modified config
     return config;
   },
 };
